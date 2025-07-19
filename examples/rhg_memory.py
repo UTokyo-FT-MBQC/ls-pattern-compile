2b05c9e27b5d36a74f664be755cf60068393eb2f
masa10-f <masa1063fuk@gmail.com>
2025-07-19 15:52:57 +0900

    add demo of RHG memory pattenr

--- examples/rhg_memory.py
+++ examples/rhg_memory.py
@@ -1,12 +1,25 @@
 """RHG memory simulation example."""
 
 # %%
+
+from graphix_zx.pattern import print_pattern
+from graphix_zx.stim_compiler import stim_compile
+
 from lspattern.rhg import create_rhg, visualize_rhg
 from lspattern.ops import memory
 
 # %%
-d = 5
-rhg_lattice, coord2node = create_rhg(d, d, d)
+d = 3
+rhg_lattice, coord2node, x, z = create_rhg(d, d, d)
 visualize_rhg(rhg_lattice, coord2node)
+
+# %%
+pattern = memory(d, d, d)
+print_pattern(pattern)
+
+# %%
+# compile to stim
+stim_str = stim_compile(pattern)
+print(stim_str)
 
 # %%
