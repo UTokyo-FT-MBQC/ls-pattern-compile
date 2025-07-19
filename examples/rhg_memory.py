d340ed66173c764d0003cc0ccd7ecace840ef72a
masa10-f <masa1063fuk@gmail.com>
2025-07-19 19:00:55 +0900

    add logical observables

--- examples/rhg_memory.py
+++ examples/rhg_memory.py
@@ -2,6 +2,7 @@
 
 # %%
 
+from operator import le
 from graphix_zx.pattern import print_pattern
 from graphix_zx.stim_compiler import stim_compile
 
@@ -9,18 +10,28 @@ from lspattern.rhg import create_rhg, visualize_rhg
 from lspattern.ops import memory
 
 # %%
-d = 5
-rhg_lattice, coord2node, x, z = create_rhg(d, d, d)
+d = 3
+rhg_lattice, coord2node, x, z = create_rhg(d)
 visualize_rhg(rhg_lattice, coord2node)
 
+length = 2 * d - 1
+logical_x = set(range(d))
+logical_z = set(length * i for i in range(d))
+print(f"logical X: {logical_x}")
+print(f"logical Z: {logical_z}")
+logical_observables = {0: logical_x, 1: logical_z}
+
 # %%
-pattern = memory(d, d, d)
+pattern = memory(d)
 print_pattern(pattern)
 
 # %%
 # compile to stim
 stim_str = stim_compile(
-    pattern, after_clifford_depolarization=0.001, before_measure_flip_probability=0.01
+    pattern,
+    logical_observables,
+    after_clifford_depolarization=0.001,
+    before_measure_flip_probability=0.01,
 )
 print(stim_str)
 
