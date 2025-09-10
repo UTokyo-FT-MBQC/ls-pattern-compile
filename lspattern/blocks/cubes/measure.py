cf7d40e00bde600795e36c8fce89dfa95cbb450e
masa10-f <masa1063fuk@gmail.com>
2025-09-10 22:03:20 +0900

    ruff

--- lspattern/blocks/cubes/measure.py
+++ lspattern/blocks/cubes/measure.py
@@ -6,7 +6,6 @@ from graphix_zx.common import Axis, AxisMeasBasis, Sign
 
 from lspattern.blocks.base import RHGBlock, RHGBlockSkeleton
 
-
 if TYPE_CHECKING:
     from lspattern.canvas import RHGCanvas
 
