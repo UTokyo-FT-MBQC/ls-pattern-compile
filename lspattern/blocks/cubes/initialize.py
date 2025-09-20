5c27e78375ced443f4190b8f42a1a065852282d9
masa10-f <masa1063fuk@gmail.com>
2025-09-20 14:11:03 +0900

    fix dangling position

--- lspattern/blocks/cubes/initialize.py
+++ lspattern/blocks/cubes/initialize.py
@@ -298,7 +298,7 @@ class InitZeroThinLayer(RHGCube):
 
         # add dangling detectors for connectivity to next block
         for x, y in x2d + z2d:
-            node_id = self.coord2node.get(PhysCoordGlobal3D((x, y, z_offset + 2 * self.d - 2)))
+            node_id = self.coord2node.get(PhysCoordGlobal3D((x, y, z_offset + 2 * self.d - 1)))
             if node_id is None:
                 continue
             dangling_detectors[PhysCoordLocal2D((x, y))] = {node_id}
