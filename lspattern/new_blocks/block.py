9bcdc4ac943ff1808cd4db329dac03c87e2acb60
masa10-f <masa1063fuk@gmail.com>
2025-10-28 23:05:50 +0900

    add properties

--- lspattern/new_blocks/block.py
+++ lspattern/new_blocks/block.py
@@ -8,10 +8,48 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from graphqomb.graphstate import GraphState
 
+    from lspattern.new_blocks.mytype import Coord2D, Coord3D
+
 
 class RHGBlock(ABC):
+    @property
     @abstractmethod
-    def materialize(self, graph: GraphState) -> tuple[GraphState, dict[tuple[int, int, int], int]]:
+    def in_ports(self) -> set[Coord2D]:
+        """Get the input ports of the block.
+
+        Returns
+        -------
+        set[Coord2D]
+            A set of input port coordinates.
+        """
+        ...
+
+    @property
+    @abstractmethod
+    def out_ports(self) -> set[Coord2D]:
+        """Get the output ports of the block.
+
+        Returns
+        -------
+        set[Coord2D]
+            A set of output port coordinates.
+        """
+        ...
+
+    @property
+    @abstractmethod
+    def cout_ports(self) -> set[Coord3D]:
+        """Get the classical output ports of the block.
+
+        Returns
+        -------
+        set[Coord3D]
+            A set of classical output port coordinates.
+        """
+        ...
+
+    @abstractmethod
+    def materialize(self, graph: GraphState) -> tuple[GraphState, dict[Coord3D, int]]:
         """Materialize the block into the given graph.
 
         Parameters
@@ -21,7 +59,7 @@ class RHGBlock(ABC):
 
         Returns
         -------
-        tuple[GraphState, dict[tuple[int, int, int], int]]
+        tuple[GraphState, dict[Coord3D, int]]
             A tuple containing the updated graph and a mapping from local coordinates to node IDs.
         """
         ...
