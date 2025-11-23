e70974db6f843835dc5d15180c8f37978d21a3fd
masa10-f <masa1063fuk@gmail.com>
2025-11-23 12:05:03 +0900

    use Enum instead of StrEnum

--- lspattern/new_blocks/mytype.py
+++ lspattern/new_blocks/mytype.py
@@ -2,21 +2,8 @@
 
 from __future__ import annotations
 
-import sys
+from enum import Enum, auto
 from typing import NamedTuple
-
-# TODO: Remove fallback once Python 3.11+ is required
-if sys.version_info >= (3, 11):  # noqa: UP036
-    from enum import StrEnum
-else:
-    try:
-        from typing_extensions import StrEnum
-    except (ImportError, AttributeError):
-        # Fallback for Python 3.10 without typing_extensions >= 4.2.0
-        from enum import Enum
-
-        class StrEnum(str, Enum):  # type: ignore[no-redef]  # noqa: UP042
-            """StrEnum fallback for Python 3.10."""
 
 
 class Coord2D(NamedTuple):
@@ -30,32 +17,29 @@ class Coord3D(NamedTuple):
     z: int
 
 
-class NodeRole(StrEnum):
+class NodeRole(Enum):
     """Role of a lattice node within the coordinate-based layer."""
 
-    DATA = "DATA"
-    ANCILLA_X = "ANCILLA_X"
-    ANCILLA_Z = "ANCILLA_Z"
+    DATA = auto()
+    ANCILLA_X = auto()
+    ANCILLA_Z = auto()
 
 
-class DIRECTION2D(StrEnum):
+class DIRECTION2D(Enum):
     """Direction in 2D lattice."""
 
-    LEFT = "LEFT"
-    RIGHT = "RIGHT"
-    TOP = "TOP"
-    BOTTOM = "BOTTOM"
+    LEFT = auto()
+    RIGHT = auto()
+    TOP = auto()
+    BOTTOM = auto()
 
 
-class AxisDIRECTION2D(StrEnum):
+class AxisDIRECTION2D(Enum):
     """Axis direction in 2D lattice."""
 
-    H = "HORIZONTAL"
-    V = "VERTICAL"
+    H = auto()
+    V = auto()
 
 
 QubitGroupId = int
 NodeId = int
-
-
-__all__ = ["Coord2D", "Coord3D", "NodeId", "NodeRole", "QubitGroupId"]
